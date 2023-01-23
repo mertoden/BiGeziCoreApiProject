2c345caa078c4692af5c02b367d88b6a0e4e655e
Mert Öden <93067736+mertoden@users.noreply.github.com>
2023-01-23 04:15:00 +0300

    Hazır Hale Getirildi.

--- BiGeziCoreApiProject/Controllers/VisitorController.cs
+++ BiGeziCoreApiProject/Controllers/VisitorController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BiGeziCoreApiProject.DAL.Context;
+using BiGeziCoreApiProject.DAL.Entities;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace BiGeziCoreApiProject.Controllers
 {
@@ -7,6 +10,74 @@ namespace BiGeziCoreApiProject.Controllers
     [ApiController]
     public class VisitorController : ControllerBase
     {
+        [HttpGet]
+        public IActionResult VisitorList()
+        {
+            using (var context = new VisitorContext())
+            {
+                var values = context.Visitors.ToList();
+                return Ok(values);
+            }
+        }
+        [HttpPost]
+        public IActionResult VisitorAdd(Visitor visitor)
+        {
+            using (var context = new VisitorContext())
+            {
+                context.Add(visitor);
+                context.SaveChanges();
+                return Ok();
+            }
+        }
+        [HttpGet("{id}")]
+        public IActionResult VisitorGet(int id)
+        {
+            using (var context = new VisitorContext())
+            {
+                var values = context.Visitors.Find(id);
+                if(values == null)
+                {
+                    return NotFound();
+                }
+                else 
+                {
+                    return Ok(values); 
+                }
+            }
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteVisitor(int id)
+        {
+            using (var context = new VisitorContext())
+            {
+                var values = context.Visitors.Find(id);
+                if(values == null) { return NotFound(); }
+                else { context.Remove(values); context.SaveChanges(); return Ok(); }
+            }
+        }
+        [HttpPut]
+        public IActionResult UpdateVisitor(Visitor visitor)
+        {
+            using (var context = new VisitorContext())
+            {
+                var values = context.Find<Visitor>(visitor.VisitorID);
+                if (values == null)
+                {
+                    return NotFound();
 
+                }
+                else 
+                {
+                    values.Name = visitor.Name;
+                    values.Surname = visitor.Surname;
+                    values.Mail = visitor.Mail;
+                    values.City = visitor.City;
+                    values.Country = visitor.Country;
+                    context.Update(values);
+                    context.SaveChanges();
+                    return Ok();
+                }
+            }
+        }
     }
 }
---
+++ BiGeziCoreApiProject/DAL/Context/VisitorContext.cs
@@ -0,0 +1,15 @@
+﻿using BiGeziCoreApiProject.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BiGeziCoreApiProject.DAL.Context
+{
+    public class VisitorContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-T5U2SAE;initial catalog=BiGeziDbApi;integrated security=true;");
+        }
+
+        public DbSet<Visitor> Visitors { get; set; }
+    }
+}
